e49128c4a927f3611240561f46d09107484ba1fc
Larry Davis <lazdnet@gmail.com>
2013-12-16 09:40:11 -0800

    Work with streams

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,27 @@
 var gulp = require('gulp');
 var karma = require('../gulp-karma/index.js');
-var es = require('event-stream');
+
+var includeOrder = [
+  'client/scripts/todo/todo.js',
+  'client/scripts/todo/todo.polyfills.js',
+  'client/scripts/todo/todo.util.js',
+  'client/scripts/todo/todo.App.js'
+];
+
+var testIncludeOrder = includeOrder.concat(['test/client/*.js']);
 
 gulp.task('test', function() {
-  return karma({
-    configFile: 'karma.conf.js',
-    singleRun: true
-  });
+  return gulp.src(testIncludeOrder)
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    }));
 });
 
-// default task gets called when you run the `gulp` command
 gulp.task('default', function() {
-  // Start the Karma server
-  return karma({
-    configFile: 'karma.conf.js',
-    background: true,
-    autoWatch: true
-  });
+  return gulp.src(testIncludeOrder)
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'watch'
+    }));
 });
