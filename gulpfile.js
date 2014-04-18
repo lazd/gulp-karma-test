bb22bbe4d65fc169fd65d770c7ae1fc73733501f
Larry Davis <lazdnet@gmail.com>
2014-04-17 21:41:30 -0700

    Clarify comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,20 @@
 var gulp = require('gulp');
 
 // Create a gulp+karma helper with a set of default options
-// In the case, the options all come from the config file
+// In this case, the options all come from the config file
 var karma = require('gulp-karma')({
   configFile: 'karma.conf.js'
 });
 
-// Run tests once, for CI
+// Run tests once
 gulp.task('test', function() {
+  // Override configuration for CI, etc
   return karma.once({
-    // CI-specific config here
     // reporters: ['coverage']
   });
 });
 
+// WATCH OPTION 1: gulp.watch style
 gulp.task('gulp-watch', function() {
   // Start a server, then, once it's ready, run tests
   karma.start().then(karma.run);
@@ -24,6 +25,7 @@ gulp.task('gulp-watch', function() {
   });
 });
 
+// WATCH OPTION 2: Karma autoWatch style
 gulp.task('karma-watch', function() {
   // Start a karma server, run tests, then watch with karma
   return karma.start({
